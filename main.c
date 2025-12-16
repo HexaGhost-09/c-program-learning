6b9e220af8636391e86a67dffc02dda98c3eddfc
HexaGhost <hexaghost-09@tutamail.com>
2025-12-16 22:48:23 +0530

    Sub Folder added

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    // Paragraph
+    printf("Hello, world! \n");
+    printf("looks good \n");
+
+    // variable decimal
+    int age = 25;
+    int year = 2025;
+    int quantity = 1;
+    
+    printf("Your age is %d \n", age);
+    printf("The year is %d \n", year);
+    printf("In %d you will be %d years old \n", year, age);
+    printf("You ordered %d items \n", quantity);
+
+    return 0;
+}
