585f0f3f96233fe41f463cf2ecadfa6daae59bd0
HexaGhost <hexaghost-09@tutamail.com>
2025-12-17 16:20:09 +0530

    double

--- subcodes/double/double.c
+++ subcodes/double/double.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(){
+
+    double pi =  3.14159;
+    double e = 2.71828;
+
+    printf("Value of Pi is %1f", pi);
+    printf("Value of e is %1f", e);
     
     return 0;
 }
