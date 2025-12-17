334bfc92269f19ded4a92ccb68275e2eb21a0aa5
HexaGhost <hexaghost-09@tutamail.com>
2025-12-17 19:07:22 +0530

    Bool

---
+++ subcodes/bool/bool.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// in bool only status True or False
+
+int main(){
+
+    // Status
+
+    bool isonline = True;
+
+    if (isonline)
+    {
+        printf("Your are Online");
+    }
+    else
+    
+
+    return 0;
+}
