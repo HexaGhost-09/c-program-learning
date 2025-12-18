e971e037c9b7ce2695a68380f0c70a7881dff997
HexaGhost <hexaghost-09@tutamail.com>
2025-12-18 21:10:46 +0530

    Format Specifiers

--- subcodes/format-specifiers/format-specifier2.c
+++ subcodes/format-specifiers/format-specifier2.c
@@ -4,11 +4,11 @@ int main(){
 
     float price1 = 23.4;
     float price2 = 242.4;
-    float price3 = 9.79;
+    float price3 = -9.79;
 
-    printf("%.2f \n",price1);
-    printf("%.2f \n",price2);
-    printf("%.2f \n",price3);
+    printf("%+7.2f \n",price1);
+    printf("%+7.2f \n",price2);
+    printf("%+7.2f \n",price3);
 
     return 0;
 }
