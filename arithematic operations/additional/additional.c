7cbd07fb948d4c81631a390d535da302d69d83c0
HexaGhost <hexaghost-09@tutamail.com>
2025-12-19 20:06:29 +0530

    Extras

--- arithematic operations/additional/additional.c
+++ arithematic operations/additional/additional.c
@@ -7,14 +7,26 @@ int main(){
     int x = 10;
     int y = 3;
     int z = 0;
+    int p = 10;
+    int q = 10;
+    int r = 10;
+    int s = 10;
 
-    // x+=3
+    // augmented assign operators are in / message or green
+
+    // x+=3 addition
     x = x+2;
-    // x-=2
+    // x-=2 subtraction
     y = y-2;
+    // x*=2 multiplication
+    p = p * 2;
+    // x/=2 division
+    q = q / 2;
 
     printf("%d \n", x);
     printf("%d \n", y);
+    printf("%d \n", p);
+    printf("%d \n", q);
     
     return 0;
 }
